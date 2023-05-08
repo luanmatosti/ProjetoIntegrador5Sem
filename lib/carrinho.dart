08d6b6d36ef986df904c9800d8511cc2176aef75
luanmatosti <luanmatos.ti@gmail.com>
2023-05-08 17:02:08 -0300

    carrinho segunda atualização

--- lib/carrinho.dart
+++ lib/carrinho.dart
@@ -1,22 +1,22 @@
-import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
 class CartView extends StatefulWidget {
   @override
   _CartViewState createState() => _CartViewState();
-
 }
 
-class _CartViewState extends State<CartView>{
+class _CartViewState extends State<CartView> {
   @override
-
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 0.0,
         backgroundColor: Colors.transparent,
         leading: IconButton(
-          icon: Icon(Icons.keyboard_arrow_left, color: Colors.black,),
+          icon: Icon(
+            Icons.keyboard_arrow_left,
+            color: Colors.black,
+          ),
           onPressed: () {},
         ),
       ),
@@ -32,23 +32,95 @@ class _CartViewState extends State<CartView>{
               ),
             ),
             SizedBox(height: 12.0),
-            Container(
-              width: 80.0,
-              height: 80.0,
-              child: Center(
-                child: Container(
+            Row(
+              children: <Widget>[
+                Container(
+                  width: 80.0,
+                  height: 80.0,
                   decoration: BoxDecoration(
-                color: Colors.grey[300],
-                image: DecorationImage(
-                  fit: BoxFit.scaleDown,
-                  image: NetworkImage("https://images.tcdn.com.br/img/img_prod/595531/tenis_ous_rafa_gomes_preto_cinza_essencial_9975_1_6b155168fc482662028d32808e8991bf.jpg"),
+                    color: Colors.green[300],
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Center(
+                    child: Container(
+                      width: 60.0,
+                      height: 60.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          fit: BoxFit.scaleDown,
+                          image: NetworkImage(
+                              "https://images.tcdn.com.br/img/img_prod/595531/tenis_ous_rafa_gomes_preto_cinza_essencial_9975_1_6b155168fc482662028d32808e8991bf.jpg"),
+                        ),
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    ),
+                  ),
                 ),
-                borderRadius: BorderRadius.circular(20.0),
-                ),
-              ),
-              
-              ),
-            )
+                SizedBox(width: 12.0),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        width: 100.0,
+                        child: Text(
+                          "Nike Air Max",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 8.0),
+                      Row(
+                        children: <Widget>[
+                          Container(
+                            width: 20.0,
+                            height: 20.0,
+                            decoration: BoxDecoration(
+                              color: Colors.grey[300],
+                              borderRadius: BorderRadius.circular(4.0),
+                            ),
+                            child: Icon(
+                              Icons.add,
+                              color: Colors.white,
+                              size: 15.0,
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Text(
+                              "1",
+                              style: TextStyle(
+                                  fontSize: 16.0, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Container(
+                            width: 20.0,
+                            height: 20.0,
+                            decoration: BoxDecoration(
+                              color: Colors.blue[300],
+                              borderRadius: BorderRadius.circular(4.0),
+                            ),
+                            child: Icon(
+                              Icons.add,
+                              color: Colors.white,
+                              size: 15.0,
+                            ),
+                          ),
+                          Spacer(),
+                          Text(
+                            "\u20b9 12,000",
+                            style: TextStyle(
+                                fontSize: 16.0, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ],
         ),
       ),
